b62b099538b3db88ca7ba5e46efcf35d00f17427
zainab <azeezzainab959@gmail.com>
2025-06-29 22:29:54 +0100

    Trigger redeploy after fixing image paths

--- src/pages/FoodList.jsx
+++ src/pages/FoodList.jsx
@@ -8,14 +8,14 @@ const foodData = [
     desc: 'A delicious bowl of hot sizzling bowl of Amala & Ewedu with proteins',
     price: 50,
     category: 'Swallow',
-    image: '../images/amala.jpg',
+    image: '/images/amala.jpg',
   },
   {
     name: 'Eba + Okra Soup',
     desc: 'One large portion of hot Eba with Okra soup + variety of protein.',
     price: 95,
     category: 'Swallow',
-    image: '../images/eba and okro.png',
+    image: '/images/eba and okro.png',
   },
   {
     name: 'Fufu + Efo riro',
@@ -29,14 +29,14 @@ const foodData = [
     desc: 'A delicious one portion of hot spicy jollof rice & Chicken Stick and tilapia fish.',
     price: 30,
     category: 'Rice',
-    image: '../images/jollof rice.png',
+    image: '/images/jollof rice.png',
   },
   {
     name: 'Moin moin + Fish + Egg',
     desc: 'One medium sized portion of freshly baked moin-moin and titus fish with Egg.',
     price: 30,
     category: 'Snacks',
-    image: '../images/moin moin.jpg',
+    image: '/images/moin moin.jpg',
   },
   {
     name: 'Wheat + Vegetable soup',
